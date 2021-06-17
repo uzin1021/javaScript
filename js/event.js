6d71fbe94693c266a4967bb03a21a08c2fc7f86d
you3554@naver.com <you3554@naver.com>
2021-06-17 09:25:00 +0900

    소스파일

--- js/event.js
+++ js/event.js
@@ -1,11 +1,19 @@
 //상세 설명 보기 클릭 -> 텍스트 보이기
 function showText(){
-    var text = document.getElementById('desc')
-    text.style.display = "block"; //수직으로 보이기
+    /* var text = document.getElementById('desc')
+    text.style.display = "block"; //수직으로 보이기 */
+    document.getElementById("desc").style.display="block";
+
+    /* var button = document.getElementById("btn");
+    button.style.display = "none"; */
+    document.getElementById("btn").style.display="none";
     }
 
 //상세 설명 닫기 클릭 -> 텍스트 가리기
 function hideText(){
     var text = document.getElementById('desc')
     text.style.display = "none";
+
+    var button = document.getElementById("btn");
+    button.style.display = "block";
 }
